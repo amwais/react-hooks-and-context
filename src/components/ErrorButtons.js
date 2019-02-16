95a50966f62ebf1befe9a2c231b138d991a68118
amirplanet <amir.waisman@planet.com>
2019-02-16 20:57:21 +0100

    v04 WIP

--- src/components/ErrorButtons.js
+++ src/components/ErrorButtons.js
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { ErrorsContext } from '../App';
 
 const errors = [ 'No Network Connection', '500 Internal server error', '404 Forbidden' ];
 
 export default function ErrorButtons() {
 	const dispatch = useContext(ErrorsContext);
+	const [ loading, setLoading ] = useState(false);
 
 	const fetchFakeError = (target) => {
 		const fakeError = errors[Math.floor(Math.random() * errors.length)];
@@ -15,19 +16,21 @@ export default function ErrorButtons() {
 		} else if (target === 'form') {
 			type = 'FETCH_FORM_ERROR';
 		}
-
+		setLoading(true);
 		setTimeout(() => {
 			dispatch({
 				type,
 				payload: fakeError
 			});
-		}, 1000);
+			setLoading(false);
+		}, 500);
 	};
 
 	return (
 		<div style={{ marginTop: '15px' }}>
 			<button onClick={() => fetchFakeError('header')}>Generate Header Error</button>
 			<button onClick={() => fetchFakeError('form')}>Generate Form Error</button>
+			{loading && <h3>Fetching Data...</h3>}
 		</div>
 	);
 }
